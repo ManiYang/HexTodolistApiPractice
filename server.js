d94b9ca5d7cdf68db793eed57923fa30408429f4
AlbertYang <33620464+ManiYang@users.noreply.github.com>
2024-04-26 19:32:51 +0800

    set up http server

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-
-
+const http = require("http");
+const { v4: uuidv4 } = require("uuid");
 
 const port = process.env.PORT || 3005;
+
+
+function httpListener(req, res)
+{
+
+}
+
+http.createServer(httpListener).listen(port);
